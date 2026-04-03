623fa780f906b58ddd2a1a428943bacce48cc285
Kamoliddin0606 <35606751+Kamoliddin0606@users.noreply.github.com>
2026-04-03 17:54:47 +0500

    Revert "updated"
    This reverts commit 1eb6daf64257d9b1d30969809d1b8fcce406c3cd.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/date_symbol_data_local.dart';
@@ -27,15 +26,14 @@ void main() async {
   // TODO: Initialize Firebase
   // await Firebase.initializeApp();
 
-  // Initialize Database (skip on web - sqflite not supported)
-  if (!kIsWeb) {
-    await sl<DatabaseHelper>().database;
-  }
+  // Initialize Database
+  await sl<DatabaseHelper>().database;
 
   // TODO: Initialize other services
 
   runApp(const App());
 }
+
 
 
 class App extends StatelessWidget {
